f4a8b91eeeb7668edc322e5d6c0d27cf05477e5b
TGates <tgates@mach-hosting.com>
2023-09-21 21:30:57 -0400

    Updated the editor and removed old files
    Converted pop-up editor to be inline and removed un-used files.

--- code/controller.ext.php
+++ code/controller.ext.php
@@ -1,28 +1,23 @@
 <?php
 /*
  * Controller script for Custom Error Pages Module for Sentora.org
- * Version : 3.1.3
+ * Version : 3.0.3
  * Author :  TGates
- * Email :  tgates@sentora.org
+ * Email :  tgates@mach-hosting.com
  */
 
 // Normal functions
 // Restore default error pages
-	function restoreDefaultPages($dst)
-	{
+	function restoreDefaultPages($dst) {
 		$errorpages = ctrl_options::GetSystemOption('static_dir') . "/errorpages/";
 		$dir = opendir($errorpages); 
 		@mkdir($dst); 
-		while (false !== ( $file = readdir($dir)) )
-		{ 
-			if (( $file != '.' ) && ( $file != '..' ))
-			{ 
-				if ( is_dir($errorpages . '/' . $file) )
-				{ 
+		while(false !== ( $file = readdir($dir)) ) { 
+			if (( $file != '.' ) && ( $file != '..' )) { 
+				if ( is_dir($errorpages . '/' . $file) ) { 
 					restoreDefaultPages($errorpages . '/' . $file,$dst . '/' . $file); 
 				} 
-				else
-				{ 
+				else { 
 					copy($errorpages . '/' . $file,$dst . '/' . $file); 
 				} 
 			} 
@@ -31,33 +26,26 @@
 	}
 
 // Function to retrieve remote XML for update check
-	function check_remote_xml($xmlurl,$destfile)
-	{
+	function check_remote_xml($xmlurl,$destfile){
 		$feed = simplexml_load_file($xmlurl);
 		if ($feed)
 		{
 			// $feed is valid, save it
 			$feed->asXML($destfile);
-		} elseif (file_exists($destfile))
-		{
+		} elseif (file_exists($destfile)) {
 			// $feed is not valid, grab the last backup
 			$feed = simplexml_load_file($destfile);
-		}
-		else
-		{
+		} else {
 			die('Unable to retrieve XML file');
 		}
 	}
 
 // Class controller & static functions
-class module_controller
-{
+class module_controller {
 	
 // Module update check functions
-    static function getModuleVersion()
-	{
+    static function getModuleVersion() {
         global $zdbh, $controller, $zlo;
-		
         $module_path="./modules/" . $controller->GetControllerRequest('URL', 'module');
         
         // Get Update URL and Version From module.xml
@@ -68,10 +56,8 @@ class module_controller
 		echo " ".$module_version."";
     }
 	
-    static function getCheckUpdate()
-	{
+    static function getCheckUpdate() {
         global $zdbh, $controller, $zlo;
-		
         $module_path="./modules/" . $controller->GetControllerRequest('URL', 'module');
         
         // Get Update URL and Version From module.xml
@@ -88,133 +74,94 @@ class module_controller
         $update_url = $update_config->document->downloadurl[0]->tagData;
         $update_version = $update_config->document->latestversion[0]->tagData;
 
-        if ($update_version > $module_version) return true;
+        if($update_version > $module_version)
+            return true;
         return false;
     }
 
 // default module static functions
-    static function getModuleDesc()
-	{
+    static function getModuleDesc() {
         $message = ui_language::translate("An error page informs a visitor when there is a problem accessing your site. Each type of problem has its own code. For example, a visitor who enters a non-existent URL will see a 404 error, while an unauthorized user trying to access a restricted area of your site will see a 403 error.<br><br>Basic error pages are automatically provided by the web server (Apache). However, if you prefer, you can create a custom error page instead of using the defaults. NOTE: Error pages are added automatically if they are found in the _errorpages directory and if they are a valid error code, and saved in the proper format: <error_number>.html (404.html)");
-		
         return $message;
     }
 
-	static function getModuleName()
-	{
+	static function getModuleName() {
 		$module_name = ui_module::GetModuleName();
-		
         return $module_name;
     }
 
-	static function getModuleIcon()
-	{
+	static function getModuleIcon() {
 		global $controller;
-		
 		$module_icon = "/modules/" . $controller->GetControllerRequest('URL', 'module') . "/assets/icon.png";
-		
         return $module_icon;
     }
 
-    static function getCSFR_Tag()
-	{
+    static function getCSFR_Tag() {
         return runtime_csfr::Token();
     }
 
 // module specific static functions
-
-    /* Load CSS and JS files */
-    static function getInit()
-	{
-        global $controller;
-		
-        $line = '<link rel="stylesheet" type="text/css" href="modules/' . $controller->GetControllerRequest('URL', 'module') . '/assets/css-pop.css">';
-        $line .= '<script type="text/javascript" src="modules/' . $controller->GetControllerRequest('URL', 'module') . '/assets/css-pop.js"></script>';
-		
-        return $line;
-    }
-	
-    static function getDomainSelected()
-	{
-        if (isset($_POST['domain']))
-		{
+    static function getDomainSelected() {
+        if (isset($_POST['domain'])) {
 			return true;
-        }
-		else
-		{
+        } else {
         	return false;
         }
     }
 
-    static function getRestorePages()
-	{
-        if (isset($_POST['restore']))
-		{
+    static function getRestorePages() {
+        if (isset($_POST['restore'])) {
 			return true;
-        }
-		else
-		{
+        } else {
         	return false;
         }
     }
 
-    static function getEditPage()
-	{
-        if (isset($_POST['pageeditor']))
-		{
-			return true;
-        }
-		else
-		{
+    static function getOpenEditor() {
+		global $controller;
+		if ($controller->GetAllControllerRequests('FORM')) {
+			$domain = $controller->GetAllControllerRequests('FORM');
+			$domain = array_shift($domain);
+			$theEditor = '
+			<iframe src="./modules/' . $controller->GetControllerRequest('URL', 'module') . '/code/editor.php?domain=' . $domain . '" name="ep_editor" width="100%" height="985" scrolling="auto" frameborder="0"></iframe>
+			';
+			//include("./modules/" . $controller->GetControllerRequest('URL', 'module') . "/code/editor.php?domain=" . $domain . "");
+			return $theEditor;
+        } else {
         	return false;
         }
     }
 
-    static function getDomVar()
-	{
-        if (isset($_POST['domain']))
-		{
+    static function getDomVar() {
+        if (isset($_POST['domain'])) {
 			$domVar = $_POST['domain'];
-			$domVar = str_replace('/', '', $domVar);
 			return $domVar;
-        }
-		else
-		{
+        } else {
         	return false;
         }
     }
 
-    static function ListDomains($uid = 0)
-	{
+    static function ListDomains($uid = 0) {
         global $zdbh;
-        if ($uid == 0)
-		{
+        if ($uid == 0) {
             $sql = "SELECT * FROM x_vhosts WHERE vh_deleted_ts IS NULL ORDER BY vh_name_vc ASC";
             $numrows = $zdbh->prepare($sql);
-        }
-		else
-		{
+        } else {
             $sql = "SELECT * FROM x_vhosts WHERE vh_acc_fk=:uid AND vh_deleted_ts IS NULL ORDER BY vh_name_vc ASC";
             $numrows = $zdbh->prepare($sql);
             $numrows->bindParam(':uid', $uid);
         }
-
         $numrows->execute();
-        if ($numrows->fetchColumn() <> 0)
-		{
-            if ($uid == 0)
-			{
+        if ($numrows->fetchColumn() <> 0) {
+            if ($uid == 0) {
                 $sql = $zdbh->prepare($sql);
-            }
-			else
-			{
+            }else{
                 $sql = $zdbh->prepare($sql);
                 $sql->bindParam(':uid', $uid);
             }
             $res = array();
             $sql->execute();
-            while ($rowdomains = $sql->fetch())
-			{
+            while ($rowdomains = $sql->fetch()) {
                 array_push($res, array(
                     'uid' => $rowdomains['vh_acc_fk'],
                     'name' => $rowdomains['vh_name_vc'],
@@ -224,35 +171,23 @@ class module_controller
                 ));
             }
             return $res;
-        }
-		else
-		{
+        } else {
             return false;
         }
     }
 
-    static function ListDomainDirs($uid)
-	{
+    static function ListDomainDirs($uid) {
         global $controller;
-		
         $currentuser = ctrl_users::GetUserDetail($uid);
         $res = array();
         $handle = @opendir(ctrl_options::GetSystemOption('hosted_dir') . $currentuser['username'] . "/public_html");
         $chkdir = ctrl_options::GetSystemOption('hosted_dir') . $currentuser['username'] . "/public_html/";
-		
-        if (!$handle)
-		{
+        if (!$handle) {
             # Log an error as the folder cannot be opened...
-			echo "Cannot Open Folder...";
-        }
-		else
-		{
-            while ($file = @readdir($handle))
-			{
-                if ($file != "." && $file != ".." && $file != "_errorpages")
-				{
-                    if (is_dir($chkdir . $file))
-					{
+        } else {
+            while ($file = @readdir($handle)) {
+                if ($file != "." && $file != ".." && $file != "_errorpages") {
+                    if (is_dir($chkdir . $file)) {
                         array_push($res, array('domains' => $file));
                     }
                 }
@@ -262,16 +197,12 @@ class module_controller
         return $res;
     }
 		
-    static function getDomainList()
-	{
+    static function getDomainList() {
         $currentuser = ctrl_users::GetUserDetail();
         $res = array();
         $domains = self::ListDomains($currentuser['userid']);
-		
-        if (!fs_director::CheckForEmptyValue($domains))
-		{
-            foreach ($domains as $row)
-			{
+        if (!fs_director::CheckForEmptyValue($domains)) {
+            foreach ($domains as $row) {
                 $status = self::getDomainStatusHTML($row['active'], $row['id']);
                 $res[] = array('name' => $row['name'],
                                'directory' => $row['directory'],
@@ -280,24 +211,17 @@ class module_controller
                                'id' => $row['id']);
             }
             return $res;
-        }
-		else
-		{
+        } else {
             return false;
         }
     }
 
-    static function getDomainStatusHTML($int, $id)
-	{
+    static function getDomainStatusHTML($int, $id) {
         global $controller;
-		
-        if ($int == 1)
-		{
+        if ($int == 1) {
             return '<td><font color="green">' . ui_language::translate('Live') . '</font></td>'
                  . '<td></td>';
-        }
-		else
-		{
+        } else {
             return '<td><font color="orange">' . ui_language::translate('Pending') . '</font></td>'
                  . '<td><a href="#" class="help_small" id="help_small_' . $id . '_a"'
                  . 'title="' . ui_language::translate('Your domain will become active at the next scheduled update.  This can take up to one hour.') . '">'
@@ -305,69 +229,49 @@ class module_controller
         }
     }
 
-    static function getCurrentDomain()
-	{
+    static function getCurrentDomain() {
         global $controller;
-		
         $domain = $controller->GetControllerRequest('URL', 'domain');
-		
         return ($domain) ? $domain : '';
     }
 	
     static function getResult()
     {
-        if (!fs_director::CheckForEmptyValue(self::$blank))
-		{
+        if (!fs_director::CheckForEmptyValue(self::$blank)) {
             return ui_sysmessage::shout(ui_language::translate("Your Domain can not be empty. Please enter a valid Domain Name and try again."), "zannounceerror");
         }
-        if (!fs_director::CheckForEmptyValue(self::$badname))
-		{
+        if (!fs_director::CheckForEmptyValue(self::$badname)) {
             return ui_sysmessage::shout(ui_language::translate("Your Domain name is not valid. Please enter a valid Domain Name: i.e. 'domain.com'"), "zannounceerror");
         }
-        if (!fs_director::CheckForEmptyValue(self::$alreadyexists))
-		{
+        if (!fs_director::CheckForEmptyValue(self::$alreadyexists)) {
             return ui_sysmessage::shout(ui_language::translate("The domain already appears to exist on this server."), "zannounceerror");
         }
-        if (!fs_director::CheckForEmptyValue(self::$error))
-		{
+        if (!fs_director::CheckForEmptyValue(self::$error)) {
             return ui_sysmessage::shout(ui_language::translate("Please remove 'www'. The 'www' will automatically work with all Domains / Subdomains."), "zannounceerror");
         }
-        if (!fs_director::CheckForEmptyValue(self::$writeerror))
-		{
+        if (!fs_director::CheckForEmptyValue(self::$writeerror)) {
             return ui_sysmessage::shout(ui_language::translate("There was a problem writting to the virtual host container file. Please contact your administrator and report this error. Your domain will not function until this error is corrected."), "zannounceerror");
         }
-        if (!fs_director::CheckForEmptyValue(self::$ok))
-		{
+        if (!fs_director::CheckForEmptyValue(self::$ok)) {
             return ui_sysmessage::shout(ui_language::translate("Changes to your domain web hosting has been saved successfully."), "zannounceok");
         }
         return;
     }
 
 // Module translation static functions - Not implemented yet	
-    static function getRestart()
-	{
+    static function getRestart() {
         $message = ui_language::translate("Select a different domain/Cancel");
-		
         return $message;
     }
-
-    static function getCopyright()
-	{
-		/* THIS COPYRIGHT NOTICE MAY NOT BE ALTERED IN ANY WAY OR REMOVED FOR ANY REASON WITHOUT WRITTEN PERMISSION OF THE AUTHOR. */
-        $copyright = '<font face="ariel" size="2">' . ui_module::GetModuleName() . ' v3.1.3 &copy; 2021-' . date("Y") . ' by <a target="_blank" href="#">TGates</a> for <a target="_blank" href="http://sentora.org">Sentora Control Panel</a>&nbsp;&#8212;&nbsp;' . ui_language::translate("Help support future development of this module and donate today!") . '</font> ';
-		
-        return $copyright;
+    static function getCopyright() {
+        $message = '<font face="ariel" size="2">'.ui_module::GetModuleName().' v3.0.3 &copy; 2013-'.date("Y").' by <a target="_blank" href="http://forums.sentora.org/member.php?action=profile&uid=2">TGates</a> for <a target="_blank" href="http://sentora.org">Sentora Control Panel</a>&nbsp;&#8212;&nbsp;Help support future development of this module and donate today!</font>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="DW8QTHWW4FMBY">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" width="70" height="21" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>';
+        return $message;
     }
-
-    static function getDonation()
-	{
-        $donation = '<br />' . ui_language::translate("Donate to module developer:") . '&nbsp;
-		<form action="https://www.paypal.com/donate" method="post" target="_blank">
-			<input type="hidden" name="hosted_button_id" value="MCDRPGAZFNEMY" />
-			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" height="25" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-			<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-		</form>';
-		
-        return $donation;
-    }
+}
 ?>
--- code/editor.php
+++ code/editor.php
@@ -1,7 +1,7 @@
 <!--
 /*
  * Editor script for Custom Error Pages Module for Sentora.org
- * Version : 3.1.2
+ * Version : 3.0.3
  * Author :  TGates
  * Email :  tgates@mach-hosting.com
  */
@@ -16,25 +16,23 @@ include('../../../dryden/ctrl/auth.class.php');
 include('../../../dryden/ctrl/users.class.php');
 include('../../../inc/dbc.inc.php');
 include('controller.ext.php');
-// uses default bootstrap
 
 session_start();
 if (isset($_SESSION['zpuid']))
 {
-	$userid = $_SESSION['zpuid'];
-	$currentuser = ctrl_users::GetUserDetail($userid);
-	$userName = $currentuser['username'];
-	$hostdatadir = ctrl_options::GetOption('hosted_dir')."".$userName;
+$userid = $_SESSION['zpuid'];
+$currentuser = ctrl_users::GetUserDetail($userid);
+$hostdatadir = ctrl_options::GetOption('hosted_dir')."".$currentuser['username'];
+$userName = $currentuser['username'];
 }
 
 // configuration
-$domVar = $_GET['domain'];
-// separate domain from path
-$domVars = explode("|", $domVar);
-$domainName = $domVars[0];
-$domainPath = $domVars[1];
+$selectedDomain = $_GET['domain'];
 $url = "/modules/error_pages/code/editor.php";
-$file = $hostdatadir."/public_html/".$domainPath."/_errorpages/";
+$file = $hostdatadir."/public_html".$selectedDomain."/_errorpages/";
+$selectedDomainName = str_replace('_', '.', $selectedDomain);
+$selectedDomainName = str_replace('/', '', $selectedDomainName);
+
 ?>
 <script src="ckeditor/ckeditor.js"></script>
 <!-- restore default pages confirm box -->
@@ -56,7 +54,6 @@ function reloadPage()
 </script>
 
 <link rel="stylesheet" href="ckeditor/ckeditor.css">
-<link href="../../../../etc/styles/Sentora_Default/global-css/bootstrap.min.css" rel="stylesheet">
 
 <?php
 // check if form has been submitted
@@ -88,8 +85,8 @@ $text = file_get_contents($file);
 <div align="center">
 <form name="editor" action="" method="post">
     <input class="btn btn-success" type="submit" value="Save" />
-    <input class="btn btn-danger" type="button" name="goback" value="Go Back" onclick="history.go(-1);" /><br />
-    <strong>File Location:</strong> /public_html/<?php echo $domainPath; ?>/_errorpages/<?php echo $_POST['page']; ?><br>
+    <input class="btn btn-warning" type="button" name="goback" value="Go Back" onclick="history.go(-1);" /><br />
+    <strong>File Location:</strong> /public_html<?php echo $selectedDomain; ?>/_errorpages/<?php echo $_POST['page']; ?><br>
     <textarea class="ckeditor" id="error_pages" name="text"><?php echo htmlspecialchars($text); ?></textarea><br />
     <input type="hidden" name="page" value="<?php echo $page; ?>"> 
 </form>
@@ -100,11 +97,11 @@ if (!isset($_POST['page']))
 	{
 	// Restore default pages
 		if (isset($_POST['restore']) && ($_POST['restore'] == "restore")) {
-			$dst = $hostdatadir."/public_html/".$domainPath."/_errorpages/";
+			$dst = $hostdatadir."/public_html".$selectedDomain."/_errorpages/";
 			restoreDefaultPages($dst);
 			echo "<div align=\"center\">";
-			echo "<h3>Default error pages restored for <strong>".$domainName."</h3>";
-			echo "<input  class=\"btn btn-success\" type=\"button\" name=\"goback\" value=\"Go Back\" onclick=\"history.go(-1);\" />";
+			echo "<h3>Default error pages restored for <strong>".$selectedDomainName."</h3>";
+			echo "<input class=\"fg-button ui-state-default ui-corner-all\" type=\"button\" name=\"goback\" value=\"Go Back\" onclick=\"history.go(-1);\" />";
 			echo "</div>";
 			exit();
 		}
@@ -114,21 +111,22 @@ if((is_dir($file)) && (count(glob($file."/*"))))
 		{
 // start
 	echo "<div align=\"center\">";
-	echo "<h2>Selected Domain: ".$domainName."</h2>";
+	echo "<h2>Selected Domain: ".$selectedDomainName."</h2>";
 	echo "<strong>Select a page to edit:</strong>
         <form name=\"pageSelect\" action=\"\" method=\"post\">
             <ul style=\"list-style-type:none;\" class=\"errorpagelist\" id=\"error1\">";
 	echo "<li><input type=\"radio\" name=\"restore\" value=\"restore\" onClick=\"confSubmit(this.form);\">Restore the Default Pages</li>";
-		$path = $hostdatadir."/public_html/".$domainPath."/_errorpages/";
+		$path=$hostdatadir."/public_html".$selectedDomain."/_errorpages/";
 		$images = scandir($path);
 		$epages = scandir($path);
 		$ignore = Array(".", "..", ".htaccess", "thumbs.db", "index.html");
 		foreach($epages as $curepage){if(!in_array($curepage, $ignore)) {
 	//echo "<li><button onclick=\"DelPage('$curepage')\">Delete</button><input type=\"radio\" name=\"page\" value=\"".$curepage."\">".$curepage."</li>";
+	$selectedDomainName = str_replace('_', '.', $selectedDomain);
 	echo "
 	<li><input type=\"radio\" name=\"page\" value=\"".$curepage."\"
 	 onclick=\"this.form.submit();\">".$curepage." 
-	<a target=\"_blank\" href=\"http://".$domainName."/_errorpages/".$curepage."\">(Click to preview)</a></li>";
+	<a target=\"_blank\" href=\"http://".$selectedDomainName."/_errorpages/".$curepage."\">(Click to preview)</a></li>";
 	};
 		}
 	echo "</ul>
@@ -137,11 +135,11 @@ if((is_dir($file)) && (count(glob($file."/*"))))
 		} else {
 // Restore default pages if not exist
 	if (isset($_POST['create']) && ($_POST['create'] == "yes")) {
-		$dst = $hostdatadir."/public_html/".$domainPath."/_errorpages/";
+		$dst = $hostdatadir."/public_html".$selectedDomain."/_errorpages/";
 		restoreDefaultPages($dst);
 		echo "<div align=\"center\">";
-		echo "<h3>Default error pages restored for ".$domainName."</h3>";
-		echo "<input class=\"btn btn-success\" type=\"button\" name=\"goback\" value=\"Go Back\" onclick=\"history.go(-1);\" />";
+		echo "<h3>Default error pages restored for ".$selectedDomainName."</h3>";
+		echo "<input class=\"fg-button ui-state-default ui-corner-all\" type=\"button\" name=\"goback\" value=\"Go Back\" onclick=\"history.go(-1);\" />";
 		echo "</div>";
 	} else {
 		if (isset($_POST['create']) && ($_POST['create'] == "no")) {
@@ -154,12 +152,12 @@ if((is_dir($file)) && (count(glob($file."/*"))))
 <div align="center">
 	<h3>
     <form name="defaults" action="" method="POST">
-    No error pages exist for <strong><?php echo $domainName; ?></strong>. Restore the defaults now?
+    No error pages exist for <strong><?php echo $selectedDomainName; ?></strong>. Restore the defaults now?
     <br />
     <input type="radio" name="create" value="yes" checked="checked">Yes
     <input type="radio" name="create" value="no">No
     <br />
-    <input class="btn btn-success" type="submit" value="Continue" />
+    <input type="submit" value="Continue" />
     </form>
     </h3>
 </div>
